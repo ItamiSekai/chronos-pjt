fd21862642244ff38103b5991b723b785302147a
Gabriel <bielgomes2012@gmail.com>
2025-09-17 12:15:01 -0300

    Container Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 import './styles/global.css';
 import './styles/theme.css';
@@ -7,18 +6,10 @@ import './styles/theme.css';
 export function App() {
   return (
     <>
-      <Heading>
-        First Component
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam
-        ut non recusandae, voluptas esse reprehenderit, laudantium culpa,
-        cupiditate deserunt delectus voluptatibus necessitatibus cumque dolorum
-        optio magnam laborum maiores. Facilis.
-      </p>
+      <Container>Logo</Container>
+      <Container>Menu</Container>
+      <Container>Form</Container>
+      <Container>Footer</Container>
     </>
   );
 }
