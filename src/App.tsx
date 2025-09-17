191b85c0d0a4049bca5d7a5b797037a3d3b2850d
Gabriel <bielgomes2012@gmail.com>
2025-09-17 10:01:29 -0300

    Understandig CSS in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-export function App(){
+import './styles/global.css';
+import './styles/theme.css';
 
-    return (
-        <>
-            <h1>Primeiro Componente React</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam ut non recusandae, voluptas esse reprehenderit, laudantium culpa, cupiditate deserunt delectus voluptatibus necessitatibus cumque dolorum optio magnam laborum maiores. Facilis.</p>
-        </>
-    );
-} 
+export function App() {
+  return (
+    <>
+      <h1>Primeiro Componente React</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam
+        ut non recusandae, voluptas esse reprehenderit, laudantium culpa,
+        cupiditate deserunt delectus voluptatibus necessitatibus cumque dolorum
+        optio magnam laborum maiores. Facilis.
+      </p>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 // Primeiro componente da pagina
-import {App} from './App';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
+    <App />
   </StrictMode>,
-)
+);
