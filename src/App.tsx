9eeccfebbee56cb93ec486111659a4cd59260a72
Gabriel <bielgomes2012@gmail.com>
2025-09-17 12:52:36 -0300

    Logo Creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Container } from './components/Container';
-
+import { Logo } from './components/Logo';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Container>Logo</Container>
+      <Container>
+        <Logo />
+      </Container>
       <Container>Menu</Container>
       <Container>Form</Container>
       <Container>Footer</Container>
